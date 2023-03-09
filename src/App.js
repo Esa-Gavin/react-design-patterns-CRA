dc90ad5ce4a32b27de7ca94a50d259c6f4c54e3a
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2023-03-09 19:58:28 +0300

    app with CRA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SplitScreen from "./SplitScreen";
 
-function App() {
+const leftHandComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ backgroundColor: "green"}}>Left!</h1>
+      {/* <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+        voluptatum qui eveniet earum totam aperiam maiores ea fuga sit suscipit
+        odit, quidem sed, corrupti obcaecati veniam, temporibus ad quis minima.
+      </p> */}
     </div>
   );
-}
+};
 
+const rightHandComponent = () => {
+  return (
+    <div>
+      <h1 style={{ backgroundColor: "red"}}>Right!</h1>
+      {/* <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, rem
+        reiciendis ipsa atque beatae fuga autem nihil voluptatibus voluptatum
+        animi est repudiandae neque voluptas ab maxime voluptate?
+        Necessitatibus, sint impedit.
+      </p> */}
+    </div>
+  );
+};
+
+function App() {
+  return <SplitScreen left={leftHandComponent} right={rightHandComponent} />
+    
+}
 export default App;
